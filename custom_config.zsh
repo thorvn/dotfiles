77ece399183e7c6615359d898506f17be525f300
thovt <admin@thovts-MacBook-Pro.local>
2017-01-05 16:20:29 +0700

    add config zsh-autosuggest :beer:

---
+++ custom_config.zsh
@@ -0,0 +1,5 @@
+############# Config zsh-autosuggestions ############
+# Corlor: https://github.com/zsh-users/zsh-autosuggestions/issues/172#issuecomment-226022265
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=22'
+# Bindkey to accept suggest
+bindkey '`' autosuggest-accept
