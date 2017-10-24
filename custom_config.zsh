e4a1109d592f031cfbd9524f9710a1f034499424
kensupermen <votantho93@gmail.com>
2017-10-24 23:35:02 +0000

    Add alias yaourt :rocket:

--- custom_config.zsh
+++
@@ -1,5 +0,0 @@
-############# Config zsh-autosuggestions ############
-# Corlor: https://github.com/zsh-users/zsh-autosuggestions/issues/172#issuecomment-226022265
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=22'
-# Bindkey to accept suggest
-bindkey '`' autosuggest-accept
