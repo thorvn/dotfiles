a816f302beb2f0a9cf9c2c98c3be16519fa4ac99
kensupermen <votantho93@gmail.com>
2018-03-11 19:23:40 +0000

    remove zsh auto suggestion

--- .install_dotfiles.sh
+++ .install_dotfiles.sh
@@ -1,11 +1,6 @@
 git clone --separate-git-dir=$HOME/.dotfiles https://github.com/kensupermen/dotfiles $HOME/dotfiles-tmp
 rm -rf ~/dotfiles-tmp/
 
-if [ ! -f ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh ]; then
-  git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-  source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
-fi
-
 # Install vim plug for neovim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
