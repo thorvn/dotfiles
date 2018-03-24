66feaa39fc68d2761d1ee15c8814d16d153f5fcd
kensupermen <votantho93@gmail.com>
2018-03-24 20:50:11 +0000

    fix install dotfiles

--- .install_dotfiles.sh
+++ .install_dotfiles.sh
@@ -1,5 +1,19 @@
-git clone --separate-git-dir=$HOME/.dotfiles https://github.com/kensupermen/dotfiles $HOME/dotfiles-tmp
-rm -rf ~/dotfiles-tmp/
+git clone --bare https://github.com/kensupermen/dotfiles $HOME/dotfiles
+function config {
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+
+mkdir -p .dotfiles-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+fi;
+
+config checkout
+config config status.showUntrackedFiles no
 
 # Install vim plug for neovim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
