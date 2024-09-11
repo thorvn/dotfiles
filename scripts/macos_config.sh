7c5ddbcd4998d5316c029abe3b749a6296c18418
thorvn <votantho93@gmail.com>
2024-09-11 10:25:06 +0700

    Add macos config

---
+++ scripts/macos_config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Set number and currency formats
+defaults write .GlobalPreferences AppleICUNumberSymbols -dict 0 "." 1 "," 10 "." 17 "."
+
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+defaults write com.apple.dock autohide-delay -float 0; defaults write com.apple.dock autohide-time-modifier -int 0;killall Dock
+
+sudo pmset -a disablesleep 1
