6a2d007ea0d296b65d80d4cedfe4c534b331f55d
thorvn <votantho93@gmail.com>
2025-09-16 17:20:51 +0700

    Refactor keybindings in Zellij config

--- config/.pryrc
+++ config/.pryrc
@@ -4,3 +4,8 @@ if defined?(PryByebug)
   Pry.commands.alias_command 'n', 'next'
   Pry.commands.alias_command 'f', 'finish'
 end
+
+if defined?(Reline)
+  # Disable WINCH signal handling in multiplexers
+  Signal.trap("WINCH", "IGNORE") if ENV['ZELLIJ'] || ENV['TMUX']
+end
--- lazyvim/.config/nvim/lua/plugins/theme.lua
+++ lazyvim/.config/nvim/lua/plugins/theme.lua
@@ -1,12 +1,7 @@
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
-
-  -- Configure LazyVim to load gruvbox
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
   },
 }
